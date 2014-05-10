9bc2558e43a82c12d004968195e7e3f756ec96df
David Evandro <davidevandro@gmail.com>
2014-05-10 17:41:12 -0300

    Added conversion from factor to date in columns Date and Time

--- plot1.R
+++ plot1.R
@@ -14,6 +14,8 @@ colNames <- names(read.table("household_power_consumption.txt", sep = ";", nrows
 ## Read data
 data <- read.table("household_power_consumption.txt", sep = ";", nrows = nRowRead, skip = nRowSkip)
 names(data) <- colNames
+data$Date <- strptime(data$Date, "%d/%m/%Y")
+data$Time <- strptime(data$Time, "%H:%M:%S")
 
 
 ## Doing plot 1
