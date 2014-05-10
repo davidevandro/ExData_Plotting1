6d130424f45304c5f212bcccc46cd47b8768f2b2
David Evandro <davidevandro@gmail.com>
2014-05-10 19:58:14 -0300

    Script that do plot3.png and the file plot3.png

--- plot3.r
+++ plot3.r
@@ -1,5 +1,6 @@
+##Function to read data - can be skipped if you want
 ## Reading data from Feb 1st 00:01:00 to Feb 3rd 00:00:00
-## that compreends the consume from days Feb 1st to Feb 2nd 
+## that compreends the consume from days Feb 1st to Feb 2nd
 
 ## 2 days, each day with 1440 minutes
 nRowRead <-  2*1440
@@ -17,15 +18,21 @@ data <- read.table("household_power_consumption.txt", sep = ";", nrows = nRowRea
 names(data) <- colNames
 data$DateTime <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
 
-## Doing plot 2
-png(file = "plot2.png", bg = "transparent", width = 480, height = 480)
+## Doing plot 3
+png(file = "plot3.png", bg = "transparent", width = 480, height = 480)
 
 #Change the locale to USA and the way that data appears in axis to show just the american week day names
 Sys.setlocale("LC_ALL","C")
 axis.Date(1, at = data$DateTime, labels = format(data$DateTime, "%a"))
 
 ## Plot without xlabel and with type equals to line
-plot(data$DateTime, data$Global_active_power, lty = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+plot(data$DateTime, data$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
+lines(data$DateTime, data$Sub_metering_2, col = "red")
+lines(data$DateTime, data$Sub_metering_3, col = "blue")
+
+## Legend with appropriate symbols (lines)
+legend("topright", lty = c(1,1,1), col = c("black", "red", "blue"), 
+       legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 
 dev.off()
 
