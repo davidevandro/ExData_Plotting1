a61411271647ea73eab2b35f37094f761189f603
David Evandro <davidevandro@gmail.com>
2014-05-10 17:52:34 -0300

    Corrected the date-time conversion

--- plot1.R
+++ plot1.R
@@ -14,9 +14,7 @@ colNames <- names(read.table("household_power_consumption.txt", sep = ";", nrows
 ## Read data
 data <- read.table("household_power_consumption.txt", sep = ";", nrows = nRowRead, skip = nRowSkip)
 names(data) <- colNames
-data$Date <- strptime(data$Date, "%d/%m/%Y")
-data$Time <- strptime(data$Time, "%H:%M:%S")
-
+data$DateTime <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
 
 ## Doing plot 1
 png(file = "plot1.png", bg = "transparent", width = 480, height = 480)
