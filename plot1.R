a0b126d873b34419d2596e48a13a4ec44b8f2820
David Evandro <davidevandro@gmail.com>
2014-05-10 17:28:19 -0300

    Script to plot1 that do plot1.png

---
+++ plot1.R
@@ -0,0 +1,22 @@
+## Reading data
+
+## 2 days, each day with 1440 minutes
+nRowRead <-  2*1440
+
+## 6 hours and 36 minutes (in 16th) + 31 - 17 + 1 = 15 days in Dec,
+## 31 days in Jan, each with 1440 minutes + one line to header
+nRowSkip <- 6*60 + 36 + 46*1440 + 1
+
+
+## Cache the column names
+colNames <- names(read.table("household_power_consumption.txt", sep = ";", nrows = 1, header = TRUE))
+
+## Read data
+data <- read.table("household_power_consumption.txt", sep = ";", nrows = nRowRead, skip = nRowSkip)
+names(data) <- colNames
+
+
+## Doing plot 1
+png(file = "plot1.png", bg = "transparent", width = 480, height = 480)
+hist(data$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+dev.off()
